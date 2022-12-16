1d4d17102307af7919c3105c8982239074f24f3d
Rubens Zandomenighi Laszlo <rubens.zandom@gmail.com>
2022-12-16 09:58:36 -0300

    Testando funções inline

--- sislin.h
+++ sislin.h
@@ -30,6 +30,27 @@ void prnVetor (real_t *vet, unsigned int n);
 void calcProxX(double **proxX,double **xAnt,double a, double **p, int n);
 void calcProxDirecBusca(double **proxDir,double **resid, double beta,double **direcAnterior, int n);
 
+// /***********************
+//  * Função que gera os coeficientes de um sistema linear k-diagonal
+//  * i,j: coordenadas do elemento a ser calculado (0<=i,j<n)
+//  * k: numero de diagonais da matriz A
+//  ***********************/
+inline double generateRandomA( unsigned int i, unsigned int j, unsigned int k )
+{
+  static double invRandMax = 1.0 / (double)RAND_MAX;
+  return ( (i==j)?(double)(k<<1) : 1.0 )  * (double)rand() * invRandMax;
+}
+
+// /***********************
+//  * Função que gera os termos independentes de um sistema linear k-diagonal
+//  * k: numero de diagonais da matriz A
+//  ***********************/
+inline double generateRandomB( unsigned int k )
+{
+  static double invRandMax = 1.0 / (double)RAND_MAX;
+  return (double)(k<<2) * (double)rand() * invRandMax;
+}
+
 
 #endif // __SISLIN_H__
 
