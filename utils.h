63121b7e77d4a2f124df6e4ad2afa5206ed456f0
Rubens Zandomenighi Laszlo <rubens.zandom@gmail.com>
2022-12-19 15:52:05 -0300

    debug func inline

--- utils.h
+++ utils.h
@@ -16,7 +16,7 @@ void inicializarMatriz(double **vet, int lin,int col);
 //  * i,j: coordenadas do elemento a ser calculado (0<=i,j<n)
 //  * k: numero de diagonais da matriz A
 //  ***********************/
-inline double generateRandomA( unsigned int i, unsigned int j, unsigned int k )
+static inline double generateRandomA( unsigned int i, unsigned int j, unsigned int k )
 {
   static double invRandMax = 1.0 / (double)RAND_MAX;
   return ( (i==j)?(double)(k<<1) : 1.0 )  * (double)rand() * invRandMax;
@@ -26,7 +26,7 @@ inline double generateRandomA( unsigned int i, unsigned int j, unsigned int k )
 //  * Função que gera os termos independentes de um sistema linear k-diagonal
 //  * k: numero de diagonais da matriz A
 //  ***********************/
-inline double generateRandomB( unsigned int k )
+static inline double generateRandomB( unsigned int k )
 {
   static double invRandMax = 1.0 / (double)RAND_MAX;
   return (double)(k<<2) * (double)rand() * invRandMax;
